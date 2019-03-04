af4e724f5630c3a6cd6e504915cfc53134c2c393
Adam Ruffa <adam.ruffa+github@gmail.com>
2019-03-03 23:08:13 -0500

    simplification.  Need to fix diagonal bug

--- haskell-practice.hsproj/practice/Leetcode/Nqueens.hs
+++ haskell-practice.hsproj/practice/Leetcode/Nqueens.hs
@@ -12,22 +12,17 @@ data Board = Board { _board  :: [[Char]]
                    , _size    :: Int
                    } deriving (Show, Eq)
                  
-nQueens :: Int -> Int -> [[[Char]]]
+nQueens  :: Int -> Int -> [[[Char]]]
 nQueens 0 _ = [[[]]]
 nQueens sizeOfBoard numQueens = map _board $ nQueens' sizeOfBoard numQueens
-
-
 nQueens' :: Int -> Int -> [Board]  
-nQueens' size num | size <= 0 = []
-                  | num < 0   = []
-                  | num == 0  = getBoard $ emptyBoard size 0
-                  | otherwise = qround 0 num $ getBoard $ emptyBoard size num
+nQueens' size num | size <= 0 || num < 0   = []
+                  | num == 0               = getBoard $ emptyBoard size 0
+                  | otherwise              = qround 0 num $ getBoard $ emptyBoard size num
                  
-
 qround :: Int -> Int -> [Board] -> [Board]
-qround round rounds boards | round < 0       = boards
-                           | round >= rounds = boards
-                           | otherwise       = (qround (round + 1) rounds) (qround' round boards)
+qround round rounds boards | round < 0 || round >= rounds = boards
+                           | otherwise = (qround (round + 1) rounds) (qround' round boards)
 qround' :: Int -> [Board] -> [Board]
 qround' _ [] = []
 qround' round boards = (foldr (++) []) . (foldr (++) []) $
@@ -37,12 +32,8 @@ qround' round boards = (foldr (++) []) . (foldr (++) []) $
 
 emptyBools :: Int -> [Bool]
 emptyBools size = replicate size False
-
-emptyBoardString :: Int -> [[Char]]
-emptyBoardString size = map (replicate size) $ replicate size '.'
-
 emptyBoard :: Int -> Int -> Maybe Board
-emptyBoard size wantedQueens =   Just $ Board (emptyBoardString size) 
+emptyBoard size wantedQueens =   Just $ Board (map (replicate size) $ replicate size '.') 
                                               (emptyBools (size * 2 - 1)) 
                                               (emptyBools size) 
                                               (emptyBools size)
@@ -55,17 +46,10 @@ diagonal size (row, col) | row < col = diagonal size (0, col - row)
                          | otherwise = diagonal size (row - col, 0)
 
 placeBool :: Int -> [Bool] -> [Bool]
-placeBool index list | index < 0 = list
-                     | index >= (length list) = list
-                     | list!!index == True = list
-                     | otherwise = list & element index .~ True
-                     
+placeBool index list | index < 0 || index >= (length list) || list!!index == True = list
+                     | otherwise = list & element index .~ True            
 placeChar :: (Int, Int) -> [[Char]] -> [[Char]]
-placeChar (row, col) board | row < 0 = board
-                           | col < 0 = board
-                           | row >= length board = board
-                           | col >= length (board!!0) = board
-                           | board!!row!!col == 'Q' = board
+placeChar (row, col) board | row < 0 || col < 0 || row >= length board || col >= length (board!!0) || board!!row!!col == 'Q' = board
                            | otherwise = (take row board)
                                          ++ [(board!!row) & element col .~ 'Q'] ++
                                          (drop (row + 1) board)
@@ -74,7 +58,6 @@ getBoard :: Maybe Board -> [Board]
 getBoard board | board == Nothing = []
                | otherwise        = (\(Just b) -> [b]) board
                                          
-
 allPositions :: Int -> [(Int, Int)]
 allPositions size | size < 0  =  []
                   | otherwise =  foldr (++) []
@@ -82,8 +65,8 @@ allPositions size | size < 0  =  []
                                                          [0..(size - 1)])
                                             [0..(size - 1)])
 
-placeQ :: Maybe Board -> (Int, Int) -> Maybe Board
-placeQ board (row, col) = board >>= (\board -> placeQ' board (row, col))
+placeQ  :: Maybe Board -> (Int, Int) -> Maybe Board
+placeQ  board (row, col) = board >>= (\board -> placeQ' board (row, col))
 placeQ' :: Board -> (Int, Int) -> Maybe Board
 placeQ' (Board _board diag r c nQueens mQueens size) (row, col) 
         | not $ canPlace base co = Nothing
@@ -97,14 +80,6 @@ placeQ' (Board _board diag r c nQueens mQueens size) (row, col)
 
 canPlace :: Board -> (Int, Int) -> Bool
 canPlace (Board _board diag r c nQueens mQueens size) (row, col)
-         | row < 0 = False
-         | col < 0 = False
-         | row >= size = False
-         | col >= size = False
-         | diag!!(diagonal size co) = False
-         | r!!row = False
-         | c!!col = False
-         | nQueens == mQueens = False
-         | otherwise = True
+         = not $ row < 0 || col < 0 || row >= size  || col >= size || diag!!(diagonal size co) || r!!row || c!!col || nQueens == mQueens
          where base  = Board _board diag r c nQueens mQueens size
                co    = (row, col)
